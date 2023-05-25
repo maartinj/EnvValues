50872f55fc3f59419df1a6ab6d748d17cd5935b6
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2023-05-25 18:24:01 +0200

    first commit: Load initial files

---
+++ EnvValues/Views/FirstView.swift
@@ -0,0 +1,51 @@
+//
+// Created for EnvValues
+// by  Stewart Lynch on 2023-01-02
+// Using Swift 5.0
+// Running on macOS 13.1
+// 
+// Folllow me on Mastodon: @StewartLynch@iosdev.space
+// Or, Twitter, if it still exits: https://twitter.com/StewartLynch
+// Subscribe on YouTube: https://youTube.com/@StewartLynch
+// Buy me a ko-fi:  https://ko-fi.com/StewartLynch
+
+
+import SwiftUI
+
+struct FirstView: View {
+    @EnvironmentObject var appStoreService: AppStoreService
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Text("Welcome")
+                    .font(.title)
+                Text("Your Current Level is \(appStoreService.receipt.rawValue)")
+                NavigationLink("Go Next View", value: true)
+                    .buttonStyle(.borderedProminent)
+                Spacer()
+            }
+            .withBackground(color: .red)
+            .navigationDestination(for: Bool.self, destination: { _ in
+                SecondView()
+            })
+            .navigationTitle("Environment Values")
+            .toolbar {
+                ToolbarItem {
+                    Button {
+                        appStoreService.makePurchase = true
+                    } label: {
+                        Image(systemName: "gearshape")
+                    }
+                }
+            }
+        }
+        .tint(.red)
+    }
+}
+
+struct FirstView_Previews: PreviewProvider {
+    static var previews: some View {
+        FirstView()
+            .environmentObject(AppStoreService())
+    }
+}
